89d51a2a83921e921dc0253c075aec716d40c9ed
Oteri Eyenike <teyenike1@gmail.com>
2024-05-20 17:06:44 +0100

    Feat: include components to entry point

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Quiz app with Xata",
+});
+</script>
+
+<template>
+  <main>
+    <div class="ctr">
+      <Questions />
+      <Result />
+      <button type="button" class="reset-btn">Reset</button>
+    </div>
+  </main>
+</template>
