69d9b1542bfa52e0ed5716278e5b03b892e1ae03
Oteri Eyenike <teyenike1@gmail.com>
2024-09-17 11:12:47 +0100

    Update: add the container class

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <header class="header">
-    <nav class="nav">
-      <a
-        href="https://github.com/Terieyenike/xata-quiz-app"
-        class="github-link"
-        target="_blank"
-        rel="noopener noreferrer">
-        View on GitHub
-      </a>
-    </nav>
+    <div class="container">
+      <nav class="nav">
+        <a
+          href="https://github.com/Terieyenike/xata-quiz-app"
+          class="github-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View on GitHub
+        </a>
+      </nav>
+    </div>
   </header>
 </template>
