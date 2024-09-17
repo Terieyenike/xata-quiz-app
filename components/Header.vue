7a2b0e046146457194dcc46aed2fe9085a3c1a49
Oteri Eyenike <teyenike1@gmail.com>
2024-09-17 10:47:30 +0100

    Feat: github link to the quiz app

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <a
+        href="https://github.com/Terieyenike/xata-quiz-app"
+        class="github-link"
+        target="_blank"
+        rel="noopener noreferrer">
+        View on GitHub
+      </a>
+    </nav>
+  </header>
+</template>
